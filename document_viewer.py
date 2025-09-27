f8b5390d2df6630b5b8dda85570d2522303114b7
sytua <seyayuta@gmail.com>
2025-09-27 15:39:24 +0900

    Add: PDF表示機能を実装

--- document_viewer.py
+++ document_viewer.py
@@ -237,7 +237,7 @@ class DocumentViewer(QMainWindow):
 
     def init_ui(self):
         """Initialize the UI components and layout."""
-        self.setWindowTitle("Document Viewer v1.0 　　　新宿山吹ICT委員会")
+        self.setWindowTitle("Document Viewer v1.1 　|   →/← : 前後のページ    |    Ctrl+→/← : 前後の文書   |   Ctrl+ +/- : ズームイン/アウト    :  新宿山吹ICT委員会")
         self.setGeometry(100, 100, 1500, 900)
 
         central_widget = QWidget()
@@ -486,12 +486,13 @@ class DocumentViewer(QMainWindow):
     def create_menu_bar(self):
         menubar = self.menuBar()
         view_menu = menubar.addMenu('表示')
-        
+        view_menu1 = menubar.addMenu('→/← : 前後のページ')        
+        view_menu2 = menubar.addMenu('Ctrl+→/← : 前後の文書')        
         actions_data = [
             ('サイドバー表示/非表示', 'Ctrl+B', self.toggle_sidebar),
             ('コントロールパネル表示/非表示', 'Ctrl+P', self.toggle_control_panel),
             ('中央表示', 'Space', self.center_view),
-            ('使い方', 'F1', self.show_usage)  # 使い方を追加
+            ('使い方', 'F1', self.show_usage) 
         ]
         
         for text, shortcut, func in actions_data:
